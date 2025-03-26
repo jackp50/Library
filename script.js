5c0b192fb8a0445913560a9a312fb998f1922e6c
jackp50 <jpearne9@gmail.com>
2025-03-26 15:32:17 -0600

    Creating a form that off a button click will display and the user can add a book to the library. Not working yet...

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 
+const container = document.querySelector("#container");
 const myLibrary = [];
 
 function Book(title, author, pages, read) {
@@ -41,8 +42,71 @@ function displayBooks() {
         bookTemplate.id = "book-template"  
     }
 }
+// button that bring up form to create new book entry
+const bookButton = document.createElement("button");
+bookButton.textContent = "Add Book"
+bookButton.id = "book-button"
+container.appendChild(bookButton);
+bookButton.addEventListener("click", () => {
+    //create form to fill out
+    const formBody = document.createElement("div");
+    container.appendChild(formBody);
+    const form = document.createElement("form");
+    formBody.appendChild(form);
+    //title input
+    const titleInput = document.createElement('input');
+    titleInput.type = 'text';
+    titleInput.name = 'title';
+    titleInput.placeholder = 'Title';
+    titleInput.required = true;
+    form.appendChild(titleInput);
+    // author input
+    const authorInput = document.createElement('input');
+    authorInput.type = 'text';
+    authorInput.name = 'author';
+    authorInput.placeholder = 'Author';
+    authorInput.required = true;
+    form.appendChild(authorInput);
+    //page count input
+    const pageInput = document.createElement('input');
+    pageInput.type = 'number';
+    pageInput.name = 'page';
+    pageInput.placeholder = 'Page Count';
+    pageInput.required = true;
+    form.appendChild(pageInput);
+    //has read input
+    const readInput = document.createElement('input');
+    readInput.type = 'checkbox';
+    readInput.name = 'read';
+    //FIXME: label for readInput
+    readInput.label = 'Have you read this book?';
+    readInput.required = true;
+    form.appendChild(readInput);
 
-const container = document.querySelector("#container");
+    //submit button
+    const submitButton = document.createElement('button');
+    submitButton.type = 'submit';
+    submitButton.textContent = 'Submit';
+    form.appendChild(submitButton);
+
+    //event listener for submit button
+    form.addEventListener("submit", (event) => {
+        // need this because I have no where to submit it
+        event.preventDefault(); 
+        const title = titleInput.value;
+        const author = authorInput.value;
+        const pages = pageInput.value;
+        const read = readInput.checked; // Returns true if checked
+        //console log
+        console.log(`Book: ${title}, Author: ${author}, Pages: ${pages}, Read: ${read}`);
+        //add to library
+        const newBook = new Book(title, author, pages, read);
+        myLibrary.push(newBook);
+        // Clear form
+        form.reset();
+    });
+})
+
 displayBooks();
 
 
